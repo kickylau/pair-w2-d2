f0f42a5a18c3c1ee878fb16d2cb32531ed401d76
WEIQI LIU <kickylau@macbook-pro.myfiosgateway.com>
2021-12-28 17:08:03 -0500

    02mymap

--- problems/02-my-map.js
+++ problems/02-my-map.js
@@ -7,6 +7,34 @@ Do not use the built in Array#map
 
 // Examples
 
+
+*******************************************************************************/
+
+
+//1. put array and a callback into arguments
+//2. set a variable for a newarray
+//3. create a for loop
+//4. create a new variable for the array index
+//5.
+
+
+let myMap = function(array,cb) {
+    let arr = [];
+    for (let i=0; i<array.length; i++){
+        let ele = array[i];
+        arr.push(cb(ele));
+    }
+
+
+    return arr;
+
+};
+
+let func = function(ele,i,array){
+    return ele;
+}
+
+
 let result1 = myMap([100, 25, 81, 64], Math.sqrt);
 console.log(result1);   // [ 10, 5, 9, 8 ]
 
@@ -14,15 +42,6 @@ let result2 = myMap(['run', 'Forrest'], function (el) {
     return el.toUpperCase() + '!';
 });
 console.log(result2);   // [ 'RUN!', 'FORREST!' ]
-*******************************************************************************/
-
-let myMap = function() {
-
-};
-
-
-
-
 
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
