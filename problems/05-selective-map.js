2dc4f6a6845f69fd85822ffc30df31f86354b483
luis castillo <luispc13@aol.com>
2021-12-28 15:28:15 -0800

    05selectiveMap

--- problems/05-selective-map.js
+++ problems/05-selective-map.js
@@ -10,6 +10,40 @@ problem.
 
 Examples:
 
+
+
+AFTER YOU ARE FINISHED WITH THIS PROBLEM, ASK FOR A CODE REVIEW
+    (optional if you already asked a question for this problem.)
+- Explain how you are using both of the callbacks in the function.
+- What do you expect each callback function to be returning?
+- How many times are you calling each callback function?
+*******************************************************************************/
+
+// input array and two callbacks 
+// output new array
+// steps 
+// 1. put array and callbacks as arguments inside selectiveMaps
+// 2. create new empty array newArr
+// 3. create a for loop that iterates through the array
+// 4. apply first callback in array[i]
+// 5. if result is true apply second callback with that index
+// 6. if result is false keep index and not apply second callback
+// 7. push index into new array
+// 8. return new array
+
+let selectiveMap = function(array, cb1, cb2) {
+    let newArr = [];
+    for (let i = 0; i < array.length; i++){
+        if(cb1(array[i]) === true){
+           newArr.push(cb2(array[i]))
+        } else newArr.push(array[i])
+    }
+    return newArr;
+};
+
+
+
+
 function isEven(n) {
     return n % 2 === 0;
 }
@@ -34,22 +68,6 @@ console.log(selectiveMap([-10, 4, 7, 6, -2, -9], isEven, flipSign));
 
 console.log(selectiveMap([-10, 4, 7, 6, -2, -9], isPositive, square));
 // [-10, 16, 49, 36, -2, -9]
-
-AFTER YOU ARE FINISHED WITH THIS PROBLEM, ASK FOR A CODE REVIEW
-    (optional if you already asked a question for this problem.)
-- Explain how you are using both of the callbacks in the function.
-- What do you expect each callback function to be returning?
-- How many times are you calling each callback function?
-*******************************************************************************/
-
-let selectiveMap = function() {
-
-};
-
-
-
-
-
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 module.exports = selectiveMap;
